dae39e45d85a1e7caf4aa0926ce52ba73522511c
DOJO <kevin11272@gmail.com>
2024-02-20 14:29:48 +0800

    feat: create store page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :products
+  
   get "dashboard", to: "pages#dashboard"
+  get "store", to: "pages#store"
+  get "about_us", to: "pages#about_us"
+
   devise_for :users, controllers:{
     registrations: "users/registrations",
     sessions: "users/sessions",
     omniauth_callbacks: "users/omniauth_callbacks"
 }
-  get "about_us", to: "pages#about_us"
 end
