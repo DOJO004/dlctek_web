22785c3f43268da47628a4ef113c2515131ab8c8
DOJO <kevin11272@gmail.com>
2024-03-05 11:36:47 +0800

    feat: create root page

--- app/javascript/controllers/burger_menu_controller.js
+++ app/javascript/controllers/burger_menu_controller.js
@@ -2,21 +2,33 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="burger-menu"
 export default class extends Controller {
-  static targets = ["menu"]
+  static targets = ["menu", "line1", "line2"]
+
   connect() {
     console.log("HI");
   }
 
   showMenu(){
-    const menu = document.getElementById("menu");
-    const hasTranslateXFull = menu.classList.contains("translate-x-full");
+    const menu = this.menuTarget;
+    const line1 = this.line1Target
+    const line2 = this.line2Target
+
+    const hasHight0 = menu.classList.contains("h-0");
     
-    if (hasTranslateXFull) {
-      menu.classList.remove("translate-x-full");
-      menu.classList.add("translate-x-0");
+    if (hasHight0) {
+      menu.classList.remove("h-0");
+      menu.classList.add("h-screen");
+      line1.classList.add("rotate-45");
+      line1.classList.add("translate-y-1.5");
+      line2.classList.add("-rotate-45");
+      line2.classList.add("-translate-y-1.5");
     }else{
-      menu.classList.add("translate-x-full");
-      menu.classList.remove("translate-x-0");
+      menu.classList.add("h-0");
+      menu.classList.remove("h-screen");
+      line1.classList.remove("rotate-45");
+      line1.classList.remove("translate-y-1.5");
+      line2.classList.remove("-rotate-45");
+      line2.classList.remove("-translate-y-1.5");
     }
     
   }
