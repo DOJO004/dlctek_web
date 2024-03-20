dd068003bd1e9c0665aaae05cf0222b307f7a97d
DOJO <kevin11272@gmail.com>
2024-03-20 13:21:19 +0800

    style: add animetion to home page core value

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "dashboard", to: "pages#dashboard"
   get "about_us", to: "pages#about_us"
   get "service_item_card", to: "pages#service_item_card"
+  get "core_value_card", to: "pages#core_value_card"
 
   post "add_cart", to: "carts#add_cart"
   delete "delete_cart_item/:id", to: "carts#delete_cart_item", as: :delete_cart_item
