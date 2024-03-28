3114606d8f7794b5b9a1e28792674df0a1af8fee
DOJO <kevin11272@gmail.com>
2024-03-28 14:53:09 +0800

    fix log out false

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,30 @@
 class PagesController < ApplicationController
-  before_action :check_role, only: [:dashboard]
-  def home
-  end
+    before_action :check_role, only: %i[dashboard]
 
-  def about_us
-  end
-
-  def service_item_card
-    
-  end
-
-  def core_value_card
-    
-  end
-
-  def dashboard
-    @products = Product.all.limit(10)
-    @users = User.all.limit(10)
-    @q = Article.ransack(params[:q])
-    @articles = @q.result.order(id: :desc).limit(10)
-  end
-
-  private
-
-  def check_role
-    if current_user.nil? || current_user.role != "admin"
-      redirect_to root_path
+    def home
     end
-  end
+
+    def about_us
+    end
+
+    def service_item_card
+    end
+
+    def core_value_card
+    end
+
+    def dashboard
+        @products = Product.all.limit(10)
+        @users = User.all.limit(10)
+        @q = Article.ransack(params[:q])
+        @articles = @q.result.order(id: :desc).limit(10)
+    end
+
+    private
+
+    def check_role
+        if current_user.nil? || current_user.role != "admin"
+            redirect_to root_path
+        end
+    end
 end
